a2aad2eea56674dbca5fdc2f6e4ede886142aa32
Adam <adam@gatherer.tech>
2023-11-10 14:39:39 +0000

    Updated region in vpc, added ASG & launch template

---
+++ ec2-with-asg.tf
@@ -0,0 +1,43 @@
+# Auto-Scaling Group with Launch Template
+
+# Launch Template
+resource "aws_launch_template" "albtf_ec2_launch_templ" {
+  name_prefix   = "albtf_ec2_launch_templ"
+  image_id      = "ami-0a1f6cc8163bdcc75"
+  instance_type = "t2.micro"
+  user_data     = filebase64("user_data.sh")
+
+  network_interfaces {
+    associate_public_ip_address = false
+    subnet_id = aws_subnet.albtf_subnet_2.id
+    security_groups = [aws_security_group.albtf_sg_for_ec2.id]
+  }
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "ALBTF-Instance"
+    }
+  }
+}
+
+# Auto-Scaling Group
+resource "aws_autoscaling_group" "albtf_asg" {
+  
+  # Number of instances
+  desired_capacity  = 1
+  max_size          = 1
+  min_size          = 1
+
+  # Connect to Target Group
+  target_group_arns = [aws_lb_target_group.albtf_alb_tg.arn]
+
+  # Create the EC2 instances in the Private Subnet
+  vpc_zone_identifier = [
+    aws_subnet.albtf_subnet_2.id
+  ]
+
+  launch_template {
+    id      = aws_launch_template.albtf_ec2_launch_templ.id
+    version = "$Latest"
+  }
+}
--- vpc-with-subnets.tf
+++ vpc-with-subnets.tf
@@ -11,7 +11,7 @@ resource "aws_subnet" "albtf_subnet_1a" {
   vpc_id                    = aws_vpc.albtf_main.id
   cidr_block                = "10.0.0.0/27"
   map_public_ip_on_launch   = true
-  availability_zone         = "us-east-1a"
+  availability_zone         = "eu-west-2"
 }
 
 # Public Subnet 2
@@ -19,7 +19,7 @@ resource "aws_subnet" "albtf_subnet_1b" {
   vpc_id                    = aws_vpc.albtf_main.id
   cidr_block                = "10.0.0.32/27"
   map_public_ip_on_launch   = true
-  availability_zone         = "us-east-1b"
+  availability_zone         = "eu-west-1"
 }
 
 # Private Subnet
@@ -27,5 +27,5 @@ resource "aws_subnet" "albtf_subnet_2" {
   vpc_id                    = aws_vpc.albtf_main.id
   cidr_block                = "10.0.1.0/27"
   map_public_ip_on_launch   = false
-  availability_zone         = "us-east-1b"
+  availability_zone         = "eu-west-1"
 }
