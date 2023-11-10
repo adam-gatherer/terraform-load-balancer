715a3c4a37dc09e6216caef447da922b6d01d1d7
Adam <adam@gatherer.tech>
2023-11-10 15:10:47 +0000

    Fixed typos in lb-with-target-group

--- lb-with-target-group.tf
+++ lb-with-target-group.tf
@@ -1,10 +1,10 @@
 # Load Balancer
-resource "aws_lb" "albtf-lb-aws_nat_gateway" {
+resource "aws_lb" "albtf-lb" {
   name                  = "albtf-lb-asg"
   internal              = false
   load_balancer_type    = "application"
   security_groups = [ aws_security_group.albtf_sg_for_elb.id ]
-  subnets = [aws_subnet.aws_subnet.albtf_subnet_1a.id, aws_subnet.albtf_subnet_1b.id]
+  subnets = [aws_subnet.albtf_subnet_1a.id, aws_subnet.albtf_subnet_1b.id]
   depends_on = [ aws_internet_gateway.albtf_gw ]
 }
 
