7b63bbfe404c53db8f84342d2169a21f5a2ee59a
Adam <adam@gatherer.tech>
2023-11-10 15:50:13 +0000

    Commented out conf, cred, prof in main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,9 @@ terraform {
 
 provider "aws" {
   region                    = "us-east-1"
-  shared_config_files       = ["/Path/to/.aws/config"] # Example path whilst I figure out how to do this correctly
-  shared_credentials_files  = ["/Path/to/.aws/credentials"] # As above
-  profile                   = "PROFILE"
+
+  # Removed to see if it will figure it out on its own
+  #shared_config_files       = ["/Path/to/.aws/config"] # Example path whilst I figure out how to do this correctly
+  #shared_credentials_files  = ["/Path/to/.aws/credentials"] # As above
+  #profile                   = "PROFILE"
 }
